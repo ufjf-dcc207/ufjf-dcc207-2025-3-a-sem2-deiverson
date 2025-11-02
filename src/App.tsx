dc77a9b798430ca7b6a9080658c871455538dfbb
DeiversonMAP <73710805+DeiversonMAP@users.noreply.github.com>
2025-11-01 21:57:52 -0300

    Criando aplicação e adicionando bibliotecas

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Canvas } from "@react-three/fiber";
+import "./App.css"; // Mantenha o CSS para o canvas ocupar a tela
+
+// Componente principal do aplicativo
+function App() {
+    return (
+        <div id="canvas-container">
+            <Canvas />
+        </div>
+    );
+}
+
+export default App;
