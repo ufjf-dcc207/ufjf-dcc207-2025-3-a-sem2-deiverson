c1645e1fd2d7557b6befb33f06212e573dfc75b8
DeiversonMAP <73710805+DeiversonMAP@users.noreply.github.com>
2025-12-07 13:52:24 -0300

    Adicionnado gerenciamento de estado do jogador e renderização do jogador no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,27 @@ import { useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Floor from "./elements/Floor";
+
+import Player from "./elements/player/Player";
+
 import { stagesList } from "./elements/Stages";
 import type { Stage } from "./elements/Types";
-
 import "./App.css";
 
 export default function App() {
     const [activeStage, setActiveStage] = useState<Stage>(stagesList[0]);
+
+    const [playerGridPos, setPlayerGridPos] = useState<[number, number]>(
+        stagesList[0].playerPosition
+    );
+
+    const [playerRotation, setPlayerRotation] = useState<number>(2);
+
+    const handleChangeStage = (stage: Stage) => {
+        setActiveStage(stage);
+        setPlayerGridPos(stage.playerPosition);
+        setPlayerRotation(2);
+    };
 
     return (
         <div className="main-container">
@@ -19,7 +33,7 @@ export default function App() {
                         <button
                             key={stage.id}
                             className={activeStage.id === stage.id ? "active" : ""}
-                            onClick={() => setActiveStage(stage)}
+                            onClick={() => handleChangeStage(stage)}
                         >
                             {stage.name}
                         </button>
@@ -34,10 +48,8 @@ export default function App() {
                     <pointLight position={[-10, -5, -10]} intensity={0.2} />
 
                     <group position={[0, 0, 0]}>
-                        <Floor
-                            grid={activeStage.floor}
-                            playerPosition={activeStage.playerPosition}
-                        />
+                        <Floor grid={activeStage.floor} />
+                        <Player gridPosition={playerGridPos} rotationIndex={playerRotation} />
                     </group>
 
                     <OrbitControls />
