8537cd319b85e542f145cf424a3e860e1a9ca4ad
DeiversonMAP <73710805+DeiversonMAP@users.noreply.github.com>
2025-12-07 13:47:15 -0300

    Player atualizado para receber dados de posição e rotação

--- src/elements/player/Player.tsx
+++ src/elements/player/Player.tsx
@@ -3,9 +3,27 @@ import Cabeca from "./Cabeca";
 import Perna from "./Perna";
 import Torso from "./Torso";
 
-export default function Player({ scale = 0.5 }: { scale?: number }) {
+type PlayerProps = {
+    scale?: number;
+    gridPosition: [number, number];
+    rotationIndex: number; // Recebe 0, 1, 2 ou 3
+};
+
+export default function Player({ scale = 0.5, gridPosition, rotationIndex }: PlayerProps) {
+    // Posição do Grid
+    const worldX = gridPosition[0];
+    const worldZ = gridPosition[1];
+
+    // Converte o índice (0-3) para radianos (0, 90, 180, 270 graus)
+    const rotationY = rotationIndex * (Math.PI / 2);
+
     return (
-        <group scale={scale}>
+        <group
+            // Aplica a posição e rotação"
+            position={[worldX, 0, worldZ]}
+            rotation={[0, rotationY, 0]}
+            scale={scale}
+        >
             <group position={[0, 2.5, 0]}>
                 {/* Cabeça */}
                 <Cabeca position={[0, 0.85, 0]} />
